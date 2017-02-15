dc677fae175f0c00f01d00970fb2a01047a73170
M.Sasithar <snazzysasi@gmail.com>
2017-02-15 10:40:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,24 @@
-var button = document.getElementById('counter');
+//counter code
 var counter=0;
-button.onClick = function()
-{
-  counter=counter+1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+var button = document.getElementById("counter");
+button.onclick=function(){
+    
+    //create a XML req
+       var request= new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+       request.onreadystatechange = function() {
+           
+         if(request.readyState===XMLHttpRequest.DONE) {
+             if(request.status===200){
+                 var counter=request.responseText;
+                 var span = document.getElementById("count");
+                 span.innerHTML = counter.toString();
+             }
+         }  
+       };
+    
+    //Making the request
+       request.open('GET','http://sasitharm.imad.hasura-app.io/counter',true);
+       request.send(null);
 };
