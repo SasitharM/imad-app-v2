c3c27d920327d72b129b22730dd0c707ae3970a2
M.Sasithar <snazzysasi@gmail.com>
2017-02-15 11:03:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,39 @@
+console.log('Loaded!');
+                    
+                    //Changing the content in html.
+                    //var element = document.getElementById('intro');
+                    
+                    //element.innerHTML='Hai I am Salih';
+                    
+                    //Animating the image to move.
+var img = document.getElementById('salih');
+var marginleft=0;
+
+function moveright() {
+if(marginleft<950) {
+     marginleft=marginleft+10;
+     img.style.marginLeft = marginleft + 'px';
+ }
+else{
+     marginleft=-900;
+     img.style.marginLeft = marginleft + 'px';
+ }
+    
+}
+
+img.onclick=function() {
+  var interval = setInterval(moveright,20);
+};
+
+
+
+
+
+
+
+
+
+
 //counter code
 var counter=0;
 var button = document.getElementById("counter");
